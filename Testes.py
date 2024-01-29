000250a3c8335ad645d1220d4908ae265dbdc77c
Bernard2307 <bernardlnogueira@outlook.com>
2024-01-29 10:41:53 -0300

    Test Map

--- Testes.py
+++ Testes.py
@@ -1,2 +1,9 @@
-name = str(input("Qual o seu nome?: "))
-print(name.strip().title())
+import math
+def area(r):
+    return math.pi * (r**2)
+
+raios = [2, 5, 8, 12]
+
+areas = map(area,raios)
+print(list(areas))
+
