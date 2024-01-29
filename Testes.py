3b619baeca640a1ba1ccf7f978e5f5707bc0566a
Bernard2307 <bernardlnogueira@outlook.com>
2024-01-29 11:17:19 -0300

    modificações simples

--- Testes.py
+++ Testes.py
@@ -7,3 +7,10 @@ raios = [2, 5, 8, 12]
 areas = map(area,raios)
 print(list(areas))
 
+#criar uma lambida e passar função para converter Celsius em Fhereient 
+# f = 9/5 * c + 32 
+
+cidades = [('berlim',29), ('san francisco',34), ('dacota johnson',31), ('limona',21)]
+c_para_f = lambda dado: (dado[0].title(), (9/5) * dado[1] + 32)
+
+print(list(map(c_para_f,cidades)))
